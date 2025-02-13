47864ef76daa3c637f10383d7800ca1209b7f280
Alaka Dahal <me@alakadahal.me>
2025-02-13 10:10:34 +0545

    created table

--- to-do-app/app/page.tsx
+++ to-do-app/app/page.tsx
@@ -1,14 +1,16 @@
-import AddTask from "./AddTask";
-
+import AddTask from "./components/AddTask";
+import TodoList from "./components/TodoList";
 
 export default function Home() {
   return (
-    <main className="max-w 4xl mx-auto mt-4">
+    <main className="bg-white max-w-md mx-auto mt-4">
 
       <div className="text-center my-5 flex flex-col gap-4">
         <h1 className="text-2xl font-bold"> ToDo App </h1>
         <AddTask />
       </div>
+      <TodoList />
+
     </main>
   )
 } 
