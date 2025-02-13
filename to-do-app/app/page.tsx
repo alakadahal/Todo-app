1fbec90f10a9021deb4c97b19f8a5ef1553a5da6
Alaka Dahal <me@alakadahal.me>
2025-02-13 21:06:14 +0545

    created todolist

--- to-do-app/app/page.tsx
+++ to-do-app/app/page.tsx
@@ -1,7 +1,11 @@
+import { get } from "http";
 import AddTask from "./components/AddTask";
 import TodoList from "./components/TodoList";
+import { getAllTodos } from "@/api";
 
-export default function Home() {
+export default async function Home() {
+  const tasks = await getAllTodos();
+  console.log(tasks);
   return (
     <main className="bg-white max-w-md mx-auto mt-4">
 
@@ -9,7 +13,7 @@ export default function Home() {
         <h1 className="text-2xl font-bold"> ToDo App </h1>
         <AddTask />
       </div>
-      <TodoList />
+      <TodoList tasks={tasks} />
 
     </main>
   )
